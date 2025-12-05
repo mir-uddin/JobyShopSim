d22eeb747c93e0947a6c5ae8cc27f1c361e42cc1
Mir Uddin <207741241+mir-uddin@users.noreply.github.com>
2025-12-05 16:52:59 -0500

    Android app module

---
+++ app/src/main/java/com/barbershopsim/app/MainFragment.java
@@ -0,0 +1,34 @@
+package com.barbershopsim.app;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/barbershopsim/app/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.barbershopsim.app;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
